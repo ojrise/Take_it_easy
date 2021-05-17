7d6e87e4a3f05c10f140e630326fbb8c8d1a9752
Ole Jørgen Rise <ojrise@stud.ntnu.no>
2021-05-17 11:47:52 +0200

    la til at han kan telle loddrett

--- Playing_logic.py
+++ Playing_logic.py
@@ -62,21 +62,55 @@ class Brett:
 
             self.points += diag_sum #Oppdaterer poengene
 
+    #lager en funksjon for å telle poeng loddrett
+    def count_loddrett(self):
+        #lager en ny liste slik at jeg kan bruke samme logikk som for diagonal_1
+        L = self.brett_liste
+        loddrett_list = [ [  L[0][0],L[1][0], L[2][0]  ], 
+                          [  L[0][1], L[1][1], L[2][1], L[3][0]  ],
+                          [  L[0][2], L[1][2], L[2][2], L[3][1], L[4][0]  ],
+                          [  L[1][3], L[2][3], L[3][2], L[4][1]  ],
+                          [  L[2][4],L[3][3], L[4][2]  ]  ]
+        #print(loddrett_list)
+
+        count = 0
+        diag_sum = 0
+        for del_list in loddrett_list:
+            if type(del_list[0]) == Brikke:
+                første_verdi_diag = del_list[0].loddrett
+            else: første_verdi_diag = -1 #Hvis det ikke ligger en brikke der bruker jeg denne logikken 
+            
+            for brikke in del_list:
+                if type(brikke) == Brikke:
+                    count = brikke.loddrett
+                else: count = 0
+
+                #sjekker om verdiene i raden er lik. Disse vil også være ulike hvis det ikke er noen brikke der    
+                if count == første_verdi_diag: 
+                    diag_sum += count
+                else:
+                    diag_sum = 0
+                    break
+
+            self.points += diag_sum #Oppdaterer poengene
 
 
 
 def main():
-    brikke1 = Brikke(1,2,8)
+    brikke1 = Brikke(9,2,8)
     brikke2 = Brikke(9,2,3)
     brikke3 = Brikke(5,2,4)
 
     brett = Brett()
     brett.add_brikke(0,0,brikke1)
     brett.add_brikke(0,1,brikke2)
-    brett.add_brikke(0,2,brikke3)
+    brett.add_brikke(1,0,brikke2)
+    brett.add_brikke(0,2,brikke2)
+    brett.add_brikke(2,0,brikke3)
 
     #print(brett.brett_liste)
     brett.count_diagonal_1()
+    brett.count_loddrett()
     print(brett.points)
 
 if __name__ == '__main__':
